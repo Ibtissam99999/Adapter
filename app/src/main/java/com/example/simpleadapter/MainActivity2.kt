7be30159188a65e011a356e6d0129e2e457c6a5f
Ibtissam99999 <ibtissamsahmoudi0@gmail.com>
2024-11-08 11:10:03 +0100

    TpEX1

---
+++ app/src/main/java/com/example/simpleadapter/MainActivity2.kt
@@ -0,0 +1,32 @@
+package com.example.simpleadapter
+
+import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
+import androidx.appcompat.app.AppCompatActivity
+
+    class MainActivity2 : AppCompatActivity() {
+        override fun onCreate(savedInstanceState: Bundle?) {
+            super.onCreate(savedInstanceState)
+            setContentView(R.layout.activity_main)
+
+            val listView: ListView = findViewById(R.id.listView)
+
+            val languages = listOf("Java", "C#", "Python", "Kotlin", "Swift", "JavaScript", "C++")
+
+            // Question 1
+             val adapter1 = ArrayAdapter(this, android.R.layout.simple_list_item_1, languages)
+             listView.adapter = adapter1
+
+            // Question 2
+
+             val adapter2 = ArrayAdapter(this, android.R.layout.simple_list_item_single_choice, languages)
+             listView.adapter = adapter2
+             listView.choiceMode = ListView.CHOICE_MODE_SINGLE
+
+            // Question 3
+            val adapter3 = ArrayAdapter(this, android.R.layout.simple_list_item_multiple_choice, languages)
+            listView.adapter = adapter3
+            listView.choiceMode = ListView.CHOICE_MODE_MULTIPLE
+        }
+    }
